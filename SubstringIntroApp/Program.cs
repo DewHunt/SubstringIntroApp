fb64581f6ef9b5e527a4f799c0be93a1c3184da1
s-sabbir <dew.fog1553@gmail.com>
2015-05-13 16:59:22 +0600

    Initial Commit for SubstringIntroApp

---
+++ SubstringIntroApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringIntroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "ALL MEN ARE CREATED EQUALE";
+
+            String s2;
+
+            s2 = s1.Substring(12, 3);
+
+            Console.WriteLine(s2);
+
+            Console.ReadKey();
+        }
+    }
+}
